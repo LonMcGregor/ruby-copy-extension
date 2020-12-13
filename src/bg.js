5b4b8c450651bf8c219d0bafe18f33f0d48635e9
lonm <LonMcGregor@users.noreply.github.com>
2020-12-13 15:53:56 +0000

    better handling of offsets/part selects

--- src/bg.js
+++ src/bg.js
@@ -17,60 +17,96 @@ function injectedScript(){
     debugger; // being in dev console prevents copy script from working
 
     // Process the elements, including only RTs when inside RUBY
-    function processElementRecursiveRTOnly(element){
+    function processElementRecursiveRTOnly(element, startel, startoff, endel, endoff){
         if(element.nodeName==="#text"){
+            if(element==startel){
+                return element.textContent.substr(startoff);
+            }
+            if(element==endel){
+                return element.textContent.substr(0, endoff);
+            }
             return element.textContent;
         } else if(element.nodeName.toUpperCase()==="RUBY") {
             let text = '';
             let children = Array.from(element.childNodes).filter(e => e.nodeName.toUpperCase()==="RT");
             children.forEach(child => {
-                text = text + processElementRecursiveRTOnly(child);
+                text = text + processElementRecursiveRTOnly(child, startel, startoff, endel, endoff);
             });
             return text;
         } else {
             let text = '';
             Array.from(element.childNodes).forEach(child => {
-                text = text + processElementRecursiveRTOnly(child);
+                text = text + processElementRecursiveRTOnly(child, startel, startoff, endel, endoff);
             });
             return text;
         }
     }
 
     // Process the elements, excluding RTs
-    function processElementRecursiveExcludeRT(element){
+    function processElementRecursiveExcludeRT(element, startel, startoff, endel, endoff){
         if(element.nodeName.toUpperCase()==="RT"){
             return "";
         } else if(element.nodeName==="#text"){
+            if(element==startel){
+                return element.textContent.substr(startoff);
+            }
+            if(element==endel){
+                return element.textContent.substr(0, endoff);
+            }
             return element.textContent;
         } else {
             let text = '';
             Array.from(element.childNodes).forEach(child => {
-                text = text + processElementRecursiveExcludeRT(child);
+                text = text + processElementRecursiveExcludeRT(child, startel, startoff, endel, endoff);
             });
             return text;
         }
     }
 
-    const processElementRecursive = BLAH; // this should be replaced when injecting script
+    // this should be replaced when injecting script
+    const processElementRecursive = BLAH;
 
     const s = window.getSelection();
     if(s.type.toLowerCase()!=="range"){
         return;
     }
     // get all selected elements
-    // todo worry about offsets/partially selected elements later
-    // TODO what if the selection is backwards
     const selectionElements = [];
-    let current = s.anchorNode;
-    while(current != s.extentNode){
-        // todo case where selection is within a single node
+    let startel;
+    let endel;
+    let startoff;
+    let endoff;
+    if(s.anchorNode.compareDocumentPosition(s.focusNode) & Node.DOCUMENT_POSITION_PRECEDING){
+        // Backwards
+        let current = s.focusNode;
+        starel = s.focusNode;
+        startoff = s.focusOffset;
+        endel = s.anchorNode;
+        endoff = s.anchorOffset;
+        // It is not reached start of selection, or a sub-node within selection
+        while(current != s.anchorNode && !(s.anchorOffset > 0 && s.anchorNode.compareDocumentPosition(current) & Node.DOCUMENT_POSITION_CONTAINS)){
+            selectionElements.push(current);
+            current = current.nextSibling;
+        }
         selectionElements.push(current);
-        current = current.nextSibling;
+    } else {
+        // Forwards
+        let current = s.anchorNode;
+        startel = s.anchorNode;
+        startoff = s.anchorOffset;
+        endel = s.focusNode;
+        endoff = s.focusOffset;
+        // It is not reached end of selection or reached a parent node of end of selection
+        while(current != s.focusNode && !(s.focusOffset> 0 && s.focusNode.compareDocumentPosition(current) & Node.DOCUMENT_POSITION_CONTAINS)){
+            selectionElements.push(current);
+            current = current.nextSibling;
+        }
+        selectionElements.push(current);
     }
     // DFS each element and remove any <rt> OR just get only <RT>s depending on the task at hand
     let selectionText = "";
     selectionElements.forEach(element => {
-        selectionText = selectionText + processElementRecursive(element);
+        selectionText = selectionText + processElementRecursive(element, startel, startoff, endel, endoff);
     })
 
     //https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
